205e27167abef144fd46217004be83368747ee85
ttucker <tracytuckeremail@gmail.com>
2021-08-26 13:13:37 -0500

    Creating authenticated routes for currentUser

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router';
 import Dashboard from './Dashboard';
 import Login from './login';
+import { AuthProvider } from './Auth';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Dashboard} />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
